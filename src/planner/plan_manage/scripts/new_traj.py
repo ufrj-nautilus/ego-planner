9d03daafeec8cfaeafef23327bb55afc09909e0c
luccagandra <luccagandra@poli.ufrj.br>
2022-03-14 01:36:52 +0100

    Working new_traj with position_hold utilization

--- src/planner/plan_manage/scripts/new_traj.py
+++ src/planner/plan_manage/scripts/new_traj.py
@@ -1,121 +1,91 @@
 #!/usr/bin/env python 
 
-print("INITIALIZING NEW_TRAJ")
-
 # Imports
 import rospy
-from geometry_msgs.msg import Transform
-from ego_planner.msg import Bspline
-from trajectory_msgs.msg import MultiDOFJointTrajectory, MultiDOFJointTrajectoryPoint
-from geometry_msgs.msg import Transform
+from quadrotor_msgs.msg import PositionCommand
+from tf.transformations import quaternion_from_euler
+from trajectory_msgs.msg import MultiDOFJointTrajectoryPoint
+from geometry_msgs.msg import Transform, Twist
 
-class BsplineConverter:
+class PositionCommandConverter:
     def __init__(self):
-      ego_planner_traj_topic = rospy.get_param('~ego_planner_traj_topic', '/planning/bspline')
+      rospy.init_node('pos_command_msg_converter')
 
-      # Publisher for UAV position control
-      traj_pub_topic = rospy.get_param('~traj_pub_topic', '/red/tracker/input_trajectory')
+      ego_planner_traj_topic = rospy.get_param('~ego_planner_traj_topic', 'planning/pos_cmd')
 
       self.counter = 0
-      
-      # Subscriber for Ego-Planner reference trajectory
-      rospy.Subscriber(ego_planner_traj_topic, Bspline, callback=self.traj_callback)
 
-      self.pub = rospy.Publisher(traj_pub_topic, MultiDOFJointTrajectory, queue_size=10)
+      # Publisher for UAV position control
+      traj_pub_topic = rospy.get_param('~traj_pub_topic', '/red/position_hold/trajectory')
+
+      # Subscriber for Ego-Planner reference trajectory
+      rospy.Subscriber(ego_planner_traj_topic, PositionCommand, callback=self.traj_callback)
+
+      self.pub = rospy.Publisher(traj_pub_topic, MultiDOFJointTrajectoryPoint, queue_size=100)
+
+      rospy.spin()
   
     def traj_callback(self, msg):
-      multi_dof_trajectory = MultiDOFJointTrajectory()
+      # PositionCommand --> MultiDOFJointTrajectoryPoint
+      traj_point = MultiDOFJointTrajectoryPoint()
 
-      print("----------NUMBER OF CALLBACKS-------------", self.counter)
+      self.counter += 1
+    
+      pose = Transform()
+      pose.translation.x = msg.position.x
+      pose.translation.y = msg.position.y
+      pose.translation.z = msg.position.z
+      quaternion = quaternion_from_euler(0, 0, msg.yaw) 
+      pose.rotation.x = quaternion[0]
+      pose.rotation.y = quaternion[1]
+      pose.rotation.z = quaternion[2]
+      pose.rotation.w = quaternion[3]
 
-      print("----------LENGTH MSG.POS_PTS----------", len(msg.pos_pts))
-      if self.counter == 0:
-        for i in range(len(msg.pos_pts)):
-          temp_point = MultiDOFJointTrajectoryPoint()
-          temp_transform = Transform()
-          temp_transform.translation.x = msg.pos_pts[i].x
-          temp_transform.translation.y = msg.pos_pts[i].y
-          temp_transform.translation.z = msg.pos_pts[i].z
-          temp_transform.rotation.w = 1.0
+      vel = Twist()
+      acc = Twist()
 
-          temp_point.transforms.append(temp_transform)
-          multi_dof_trajectory.points.append(temp_point)
+      traj_point.transforms.append(pose)
+      traj_point.velocities.append(vel)
+      traj_point.accelerations.append(acc)
+      
+      traj_point.time_from_start.secs = 0
+      traj_point.time_from_start.nsecs = 0
 
-        self.counter += 1
-        self.pub.publish(multi_dof_trajectory)
+      print("Counter: ", self.counter)
+      print("Msg.position.x: ", msg.position.x)
+
+      self.pub.publish(traj_point)
+      
 
 if __name__ == '__main__':
-    rospy.init_node('bspline_msg_converter')
-    print("INIT NODE")
-    obj = BsplineConverter()
-    rospy.spin()
+    obj = PositionCommandConverter()
 
-""" rostopic echo -n1 /planning/bspline - 
+""" rostopic echo -n1 /planning/pos_cmd - 
 
-Type: ego_planner/Bspline
+Type: PositionCommand
 
-Publishers: 
- * /ego_planner_node (http://lucca-SVF15213CBW:33761/)
-
-Subscribers: 
- * /traj_server (http://lucca-SVF15213CBW:43621/)
-
-
-order: 3
-traj_id: 1
-start_time: 
-  secs: 3358
-  nsecs: 179000000
-knots: [-0.5120709584384695, -0.3413806389589797, -0.17069031947948984, 0.0, 0.17069031947948984, 0.3413806389589797, 0.5120709584384695, 0.6827612779179594, 0.8534515973974492, 1.024141916876939, 1.1948322363564288, 1.3655225558359185, 1.5362128753154083, 1.706903194794898, 1.8775935142743878, 2.0482838337538776]
-pos_pts: 
-  - 
-    x: -9.992398693109015
-    y: 0.013144624961962463
-    z: 0.542364572647488
-  - 
-    x: -9.9922284266492
-    y: 0.013439651271538181
-    z: 0.5424169259163675
-  - 
-    x: -9.992056722245646
-    y: 0.013737169286292502
-    z: 0.5424697213769454
-  - 
-    x: -9.934990870065192
-    y: 0.11777821581521175
-    z: 0.5615226333884763
-  - 
-    x: -9.806907339922425
-    y: 0.32022073369783033
-    z: 0.6081696456512161
-  - 
-    x: -9.620312771805821
-    y: 0.614886259402226
-    z: 0.6806909701645365
-  - 
-    x: -9.403544037976967
-    y: 0.9949890230437746
-    z: 0.7682413418313626
-  - 
-    x: -9.188916623549845
-    y: 1.3732977597632217
-    z: 0.8552829970842278
-  - 
-    x: -9.004042898005176
-    y: 1.6660749130527681
-    z: 0.9286413514044982
-  - 
-    x: -8.868805794646896
-    y: 1.8665415007017852
-    z: 0.9807512839957077
-  - 
-    x: -8.807528660448924
-    y: 1.9677008047990951
-    z: 1.0045883876850945
-  - 
-    x: -8.868635952505063
-    y: 1.866850057196867
-    z: 0.980794827258877
-yaw_pts: []
-yaw_dt: 0.0
+header: 
+  seq: 998
+  stamp: 
+    secs: 1636749300
+    nsecs: 458358945
+  frame_id: "world"
+position: 
+  x: -9.28099878117878
+  y: -8.688725040383979
+  z: 0.9997694852660133
+velocity: 
+  x: 0.0
+  y: 0.0
+  z: 0.0
+acceleration: 
+  x: 0.0
+  y: 0.0
+  z: 0.0
+yaw: -1.364799524977911
+yaw_dot: 0.0
+kx: [5.7, 5.7, 6.2]
+kv: [3.4, 3.4, 4.0]
+trajectory_id: 9
+trajectory_flag: 1
 ---"""
